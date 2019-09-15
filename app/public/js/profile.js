2eddc753be5b3d2720a0fb831bc7734f30a043a9
Terry Bae <junhbae@iu.edu>
2019-09-15 16:26:08 -0400

    javascript update and draft code for fetching

---
+++ app/public/js/profile.js
@@ -0,0 +1,18 @@
+var waitingProfile = new Vue({
+  el: '#user',
+  data: {
+    user: []
+  },
+  methods: {
+    fetchUser() {
+      fetch('https://randomuser.me/api')
+      .then(response => response.json())
+      .then (json => {waitingProfile.user = json.results[0]});
+    }
+  },
+
+  created() {
+    this.fetchUser()
+  }
+
+});
